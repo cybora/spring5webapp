efc46c0f36fb78ea24d9e9b04d0521180590a561
Bora <borakoksel@outlook.com>
2017-10-19 16:38:11 +0300

    asdadadsa

---
+++ src/main/java/com/bora/repositories/PublisherRepository.java
@@ -0,0 +1,9 @@
+package com.bora.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.bora.model.Publisher;
+
+public interface PublisherRepository extends CrudRepository<Publisher, Long>{
+
+}
